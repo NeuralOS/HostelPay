0a515f93975687b414114f96dcb532dbe141b7de
Rakhi15 <mail2k.rakhi@gmail.com>
2020-03-23 18:20:09 +0530

    Trail

--- app/src/main/java/com/oakspro/hostelpay/MainActivity.java
+++ app/src/main/java/com/oakspro/hostelpay/MainActivity.java
@@ -2,16 +2,56 @@ package com.oakspro.hostelpay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private  static int SPLASH_SCREEN=5000;
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // RAJESH update
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
         //new code
+
+
+        //Animation
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hook
+        image=findViewById(R.id.imageView);
+        logo=findViewById(R.id.textView);
+        slogo=findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogo.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
